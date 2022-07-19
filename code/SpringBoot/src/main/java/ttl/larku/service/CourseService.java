ac297ad7a20885931dc337f1356ebdd501102de9
whynot <anil@panix.com>
2022-07-19 10:20:11 -0400

    Added Modules

--- code/SpringBoot/src/main/java/ttl/larku/service/CourseService.java
+++ code/SpringBoot/src/main/java/ttl/larku/service/CourseService.java
@@ -1,6 +1,5 @@
 package ttl.larku.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import ttl.larku.dao.BaseDAO;
 import ttl.larku.domain.Course;
 
@@ -8,36 +7,42 @@ import java.util.List;
 
 public class CourseService {
 
-    //TODO - Dependency Injection needed here.
-//    @Autowired
     private BaseDAO<Course> courseDAO;
 
     public Course createCourse(String code, String title) {
         Course course = new Course(code, title);
+        course = createCourse(course);
+
+        return course;
+    }
+
+    public Course createCourse(Course course) {
         course = courseDAO.create(course);
 
         return course;
     }
 
-    public void deleteCourse(int id) {
+    public boolean deleteCourse(int id) {
         Course course = courseDAO.get(id);
         if (course != null) {
             courseDAO.delete(course);
+            return true;
         }
+        return false;
     }
 
-    public void updateCourse(Course course) {
-        courseDAO.update(course);
+    public boolean updateCourse(Course newCourse) {
+        Course oldCourse = courseDAO.get(newCourse.getId());
+        if(oldCourse != null) {
+            courseDAO.update(newCourse);
+            return true;
+        }
+        return false;
     }
 
     public Course getCourseByCode(String code) {
-        List<Course> courses = courseDAO.getAll();
-        for (Course course : courses) {
-            if (course.getCode().equals(code)) {
-                return course;
-            }
-        }
-        return null;
+        List<Course> courses = courseDAO.findBy(c -> c.getCode().equals(code));
+        return courses.size() > 0 ? courses.get(0) : null;
     }
 
     public Course getCourse(int id) {
